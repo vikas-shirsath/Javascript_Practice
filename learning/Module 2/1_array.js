aa592eb3a271e7c09783519093e7487bfb2b8d14
Vikas Pandharinath Shirsath <vikasshirsath04@gmail.com>
2024-07-18 14:32:33 +0530

    practice

---
+++ learning/Module 2/1_array.js
@@ -0,0 +1,20 @@
+//Array provides ordered collection of data
+
+var arr = [12, 'Ferrari', true, 12.48];
+
+console.log(arr);
+
+
+//Accessing elements
+var a = arr[1];
+console.log(a);
+console.log(arr[1]);
+
+//repacing by index
+arr[1] = 'Audi';
+console.log(arr[1]);
+
+//geting length of an array
+console.log(arr.length);
+
+
---
+++ learning/Module 2/2_array.js
@@ -0,0 +1,24 @@
+//methods in array (inbuilt in js)
+arr2 = [12, 14, 56, 57];
+console.log(arr2);
+
+// pop method
+var c = arr2.pop();
+console.log(c);
+console.log('poped array ' + arr2);
+
+
+//push method
+arr2.push(100);
+console.log('pushed array ' + arr2);
+
+
+//shift method
+var d = arr2.shift();
+console.log(d);
+console.log('shifted array ' + arr2);
+
+
+//unshift method
+arr2.unshift(102);
+console.log('unshifted array ' + arr2);
