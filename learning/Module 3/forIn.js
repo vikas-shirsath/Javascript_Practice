78ea88dbc8f765b90cf4c3da5124cef2bcc250b3
vikas-shirsath <vikasshirsath04@gmail.com>
2024-07-19 14:51:04 +0530

    practice

--- learning/Module 3/forIn.js
+++ learning/Module 3/forIn.js
@@ -1,1 +1,21 @@
-//
+//In javscript for in loop allows you to loop through properties of an object.
+//the statements of code found within the loop body will be executed once for each property of an object.
+
+
+var colors = {
+    primary : 'Blue',
+    secondary : 'Red',
+    tertiary : 'White'
+}
+
+
+for(var color in colors) {
+    console.log(color + ' --> ' + colors[color]);
+}
+
+
+
+var colorArray = ['Green', 'Yellow', 'Red'];
+for (var color in colorArray) {
+    console.log(color + ' --> ' + colorArray[color]);
+}
